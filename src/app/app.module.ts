225ffd770367a137b254620af4b8695b2efe5c72
Shuvankar Sarkar <sonu041@gmail.com>
2023-04-16 22:28:49 +0530

    Added navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,13 @@ import { AppComponent } from './app.component';
 import { KnowledgesComponent } from './knowledges/knowledges.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+
 
 @NgModule({
   declarations: [
@@ -25,7 +32,13 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
         allowedUrls: ['http://localhost:8080/api'],
         sendAccessToken:true
       }
-    })
+    }),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,50 @@
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { delay, filter } from 'rxjs/operators';
+import { NavigationEnd, Router } from '@angular/router';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+
+@UntilDestroy()
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+
+  constructor(private oauthService: OAuthService,
+    private observer: BreakpointObserver,
+    private router: Router) {
+  }
+
+  ngAfterViewInit() {
+    this.observer
+      .observe(['(max-width: 800px)'])
+      .pipe(delay(1), untilDestroyed(this))
+      .subscribe((res) => {
+        if (res.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      });
+
+    this.router.events      
+      .subscribe(() => {
+        if (this.sidenav.mode === 'over') {
+          this.sidenav.close();
+        }
+      });
+  }
+
+  logout() {
+    this.oauthService.logOut();
+  }
+
+}
